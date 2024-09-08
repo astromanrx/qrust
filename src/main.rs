49fdd58eb95e3bc055f214d3a244e1ab9201101c
astromanrx <astromanrx@gmail.com>
2024-09-08 06:07:58 +0330

    better tab control added.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io;
-use crossterm::cursor::{position, EnableBlinking, Hide, MoveLeft, MoveRight, MoveTo, RestorePosition, SavePosition, Show};
+use crossterm::cursor::{self, position, EnableBlinking, Hide, MoveLeft, MoveRight, MoveTo, RestorePosition, SavePosition, Show};
 use crossterm::terminal::{self, Clear, ClearType};
 use crossterm::event::{KeyEvent, KeyEventKind, KeyModifiers};
 use crossterm::{
@@ -21,7 +21,7 @@ fn main() -> io::Result<()> {
     _ = execute!(io::stdout(),Clear(ClearType::All),MoveTo(0,0));
     _ = execute!(
         io::stdout(),    
-        EnableBlinking,    
+        EnableBlinking,
     );
 
     let mut editor = Editor{ content : vec![]};
@@ -99,7 +99,8 @@ impl Editor {
     }    
 
     fn update(&self){
-        _ = execute!(io::stdout(),SavePosition,Hide,Clear(ClearType::All),MoveTo(0,0),Print(self.content.join(LINE_ENDING)),RestorePosition,Show)
+        let s= self.content.join(LINE_ENDING);
+        _ = execute!(io::stdout(),SavePosition,Hide,Clear(ClearType::All),MoveTo(0,0),Print(s),RestorePosition,Show)
     }
 
     fn save(&self){
@@ -125,7 +126,7 @@ impl Editor {
         }else if event.modifiers != KeyModifiers::ALT {
             match event.code {
                 KeyCode::Char(c) =>{
-                    self.insert_character(&c.to_string());
+                    self.insert_string(&c.to_string());
                     self.update()
                 },
                 KeyCode::Right => {
@@ -160,17 +161,26 @@ impl Editor {
                     }                    
                 },
                 KeyCode::Enter => {                    
-                    let (cursor_col,cursor_row) = position().unwrap();
-                    self.split_line(cursor_row as usize,cursor_col as usize);                    
-                    _ = execute!(io::stdout(),MoveTo(0,cursor_row+1));                    
+                    let (cursor_col,cursor_row) = position().unwrap();                                                            
+                    match self.get_line_leading_spaces(cursor_row as usize){
+                        Some(spaces) if spaces>0 => {
+                            self.split_line(cursor_row as usize,cursor_col as usize," ".repeat(spaces as usize).as_ref());
+                            _ = execute!(io::stdout(),MoveTo(spaces,cursor_row + 1))
+                        },
+                        _ => {
+                            self.split_line(cursor_row as usize,cursor_col as usize,"");
+                            _ = execute!(io::stdout(),MoveTo(0,cursor_row + 1))                            
+                        }
+                    }                    
                     self.update();
                 },
                 KeyCode::Tab =>{                    
-                    self.insert_character(&" ".repeat(TAB_SIZE));
+                    self.insert_string(&" ".repeat(TAB_SIZE));
                     self.update()
                 }
                 KeyCode::Backspace =>{
-                    let (cursor_col,cursor_row) = position().unwrap();                                                                                      
+                    let (cursor_col,cursor_row) = position().unwrap();
+                    let mut delete_length = 0;
                     if cursor_col == 0 {
                         if cursor_row > 0 {
                             let previous_line = cursor_row - 1;
@@ -178,10 +188,23 @@ impl Editor {
                             _ = execute!(io::stdout(),MoveTo(self.content[previous_line as usize].len() as u16 + 1, previous_line));
                         }                        
                     }else{
-                        self.content[cursor_row as usize] = remove_char((cursor_col - 1) as usize,cursor_col as usize,&self.content[cursor_row as usize]);
+                        match self.get_line_leading_spaces(cursor_row as usize) {
+                            Some(spaces_count) if cursor_col <= spaces_count =>{
+                                let r = spaces_count % TAB_SIZE as u16 ;
+                                if r > 0{
+                                    delete_length = r
+                                }else{
+                                    delete_length = TAB_SIZE as u16
+                                }
+                            },
+                            _ =>delete_length = 1
+                        } 
+                        self.content[cursor_row as usize] = remove_char((cursor_col - delete_length) as usize,cursor_col as usize,&self.content[cursor_row as usize]);                                                
                     }
-                    _ = execute!(io::stdout(),MoveLeft(1));
+                    
+                    _ = execute!(io::stdout(),MoveLeft(delete_length));
                     self.update();
+                    
                                                         
                 },
                 KeyCode::Delete =>{               
@@ -195,19 +218,9 @@ impl Editor {
                 },                    
                 KeyCode::Home =>{
                     let (_,cursor_row) = position().unwrap();
-                    let line = &self.content[cursor_row as usize];
-                    let mut iter = line.chars();
-                    let mut i = 0;
-                    loop{                                                                                                
-                        match iter.next(){
-                            Some(c) if ! char::is_whitespace(c)=>{                                
-                                _ = execute!(io::stdout(),MoveTo(i as u16 ,cursor_row as u16));
-                                break;
-                            },                            
-                            None=> break,
-                            _=>()
-                        }                  
-                        i += 1;       
+                    match self.get_line_leading_spaces(cursor_row as usize){
+                        None => (),
+                        Some (spaces_count)=>_ = execute!(io::stdout(),MoveTo(spaces_count ,cursor_row as u16))
                     }
                 },
                 KeyCode::End =>{
@@ -237,6 +250,22 @@ impl Editor {
         true
     }    
 
+    fn get_line_leading_spaces(&mut self,line_index: usize) -> Option<u16>{
+        let line = &self.content[line_index];
+        let mut iter = line.chars();
+        let mut i = 0;
+        loop{                                                                                                
+            match iter.next(){
+                Some(c) if ! char::is_whitespace(c)=>{                                
+                    return Some(i)
+                },                            
+                None=> return Some(i),
+                _=>()
+            }                  
+            i += 1;       
+        }
+    }
+
     fn merge_lines(&mut self,line_index: u16){
         let mut this_line = self.content[(line_index) as usize].clone();
         let next_line = self.content[(line_index + 1) as usize].as_str();                        
@@ -245,7 +274,7 @@ impl Editor {
         self.content.remove((line_index + 1) as usize);
     }
 
-    fn insert_character(&mut self, ch: &str){
+    fn insert_string(&mut self, ch: &str){
         let (cursor_col,cursor_row) = position().unwrap();     
         let current_line = cursor_row as usize;
         if self.content[current_line].len() == 0{
@@ -260,16 +289,18 @@ impl Editor {
         _ = execute!(io::stdout(),MoveRight(ch.len() as u16));                
     }
 
-    fn split_line(&mut self,line_index: usize,character_index: usize){        
+    fn split_line(&mut self,line_index: usize,character_index: usize,new_line_prefix: &str ){        
         let this_line = self.content[line_index].clone();                
         if this_line.len() == 0 {
             self.content.insert(line_index + 1,String::from(""));    
         }else{
             let (first_part,second_part) = this_line.split_at(character_index);
             self.content[line_index] = String::from(first_part);
-            self.content.insert(line_index + 1,String::from(second_part));
+            let mut s = String::from(second_part);
+            s.insert_str(0,new_line_prefix);
+            self.content.insert(line_index + 1,s);
             self.content.pop();
-        }        
+        }
     }
 }
 
